1888e4e1c41dc622ad8f189c5abfeed0c34bb4f9
AlexandreDFA <a106862@uminho.pt>
2024-10-31 14:46:22 +0000

    funcionalidades b(search) e e(criar-csv).Preciso alterar funcoes para ter mais um parametro para poder guardar o csv

--- jobscli.py
+++ jobscli.py
@@ -21,5 +21,44 @@ def request_api(metodo: str, params: dict) -> dict:
         typer.echo(f"Erro ao acessar a API: {response.status_code}")
         return {}
 
+#e)Para cada uma das funcionalidades (a), (b) e (d) deve poder exportar para CSV a informacao com os seguintes campo:
+#titulo;empresa;descricao;data_de_publicacao;salario;localizacao.
+def cria_csv(dados, nome_arquivo='jobs.csv'):
+    with open(nome_arquivo, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        
+        writer.writerow(['titulo', 'empresa', 'descricao', 'data_de_publicacao', 'salario', 'localizacao'])
+        
+        for job in dados.get('results', []):
+            descricao_limpa = re.sub(r'<.*?>', '', job.get('body', ''))
+            
+            salario = job.get('wage', 'não_encontrado') if job.get('wage') is not None else ' '
+            
+            linha = [
+                job.get('title', ''),
+                job.get('company', {}).get('name', ''),
+                descricao_limpa,
+                job.get('publishedAt', ''),
+                salario,
+                ', '.join(location['name'] for location in job.get('locations', []))
+            ]
+            
+            writer.writerow(linha)
+    
+    typer.echo(f"Dados exportados para {nome_arquivo}")
+
+#b)Listar todos os trabalhos do tipo full-time, publicados por uma determinada empresa, numa determinada localidade e determinado numero.
+#python jobscli.py search Braga EmpresaX 4
+@app.command()
+def search(localidade: str, empresa: str, limit: int, csv: bool = False):
+    params={
+        'q': f"{localidade},{empresa}",
+        'limit': limit,
+        'type': '1' 
+    }
+    response=request_api('search',params)
+    print(response)
+    if csv:
+        cria_csv(response)
     
 app()
